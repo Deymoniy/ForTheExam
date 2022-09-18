3e0653749b076117e84439e086112fbc0d5f800a
Yegor L <mmarf@inbox.ru>
2022-09-19 00:17:56 +0600

    Count was created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's see how many word in this array has 3 letters or less.");
+string[] arrayWords = { "Hello", "How", "are", "you", "today", "I", "hope", "well" };
+
+int Count(string[] array)
+{
+    int x = 0;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        x = array[i].Length;
+        if (x <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine($"In this array {Count(arrayWords)} word(s) has/have 3 letters");
+Console.WriteLine($"Now let's put them in a seperate array.");
